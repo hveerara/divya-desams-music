b75b11bb9358fdab4bd52b7581e284085bbae06e
Hema Veeraragavathatham <hema.ragavan@gmail.com>
2024-03-18 15:24:04 -0700

    Initial app scaffolding

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+
+const App = () => {
+	return <h1 className="text-primary text-4xl font-bold">Hello, World!</h1>;
+};
+
+export default hot(module)(App);
